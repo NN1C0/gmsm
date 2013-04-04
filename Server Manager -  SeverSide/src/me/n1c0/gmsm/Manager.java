961915d0f6367bb1b9298846fc6d9f305ff13658
NN1C0 <nicomeier3@live.de>
2013-04-04 23:02:26 +0200

    First unstable

---
+++ Server Manager -  SeverSide/src/me/n1c0/gmsm/Manager.java
@@ -0,0 +1,35 @@
+package me.n1c0.gmsm;
+
+import me.n1c0.gmsm.network.TCPServer;
+import me.n1c0.gmsm.player.PlayerOnlineStateHandle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Manager  extends JavaPlugin  {
+	
+	private PlayerOnlineStateHandle playerJoinHandle;
+	private TCPServer tcpServer = new TCPServer();
+	private Thread tcpServerThread = new Thread(tcpServer);
+	public void onEnable() {
+		registerEvents();
+		System.out.println("Manager wurde geladen");
+		
+		try {
+			tcpServerThread.start();
+		} catch (Exception e) {
+			System.out.println("Problem mit dem TCPServer");
+			e.printStackTrace();
+		}
+		System.out.println("TcpServer fertig");
+		
+	}
+	
+	public void onDisable() {
+		tcpServerThread.interrupt();
+	}
+
+	
+	private void registerEvents() {
+		playerJoinHandle = new PlayerOnlineStateHandle(this);
+	}
+}
